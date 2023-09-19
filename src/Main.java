26a5feefff22e1841c11fd4e1a0eee95cc8160de
Mihail <mohonovproduction@yandex.ru>
2023-09-19 15:20:44 +0300

    Mironov fix!!!

--- src/Main.java
+++ src/Main.java
@@ -2,41 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-         * 1. get cost -> pounds, shillings, pennies
-         * 2. get insert -> pounds, shillings, pennies
-         * 3. All to pennies
-         * if cost > insert -> err
-         * if cost = insert -> haven't change
-         * if cost < insert -> 4. output change in po, sh, pe,
-         *
-         * 3. po * 12 * 20 + sh * 20 + pe
-         * 4.
-         * n_pe = pe % 20
-         * n_sh = pe / 20 % 12
-         * n_po = pe / 20 / 12
-         */
-
-        Scanner sc = new Scanner(System.in);
+        //Scanner sc = new Scanner(System.in);
         int
-                cost_po = sc.nextInt(),
-                cost_sh = sc.nextInt(),
-                cost_pe = sc.nextInt(),
-                insert_po = sc.nextInt(),
-                insert_sh = sc.nextInt(),
-                insert_pe = sc.nextInt();
+                cost_po = 1, //sc.nextInt(),
+                cost_sh = 6, //sc.nextInt(),
+                cost_pe = 12, //sc.nextInt(),
+                insert_po = 2, //sc.nextInt(),
+                insert_sh = 0, //sc.nextInt(),
+                insert_pe = 0; //sc.nextInt();
         String result;
 
         cost_pe = cost_po * 12 * 20 + cost_sh * 20 + cost_pe;
         insert_pe = insert_po * 12 * 20 + insert_sh * 20 + insert_pe;
 
-        if (cost_pe < insert_pe)
-            result = "Сдача > pounds: " + ((insert_pe - cost_pe) / 20 / 12)
+        System.out.println( (cost_pe < insert_pe) ?
+            "Сдача > pounds: " + ((insert_pe - cost_pe) / 20 / 12)
                     + " shillings: " + ((insert_pe - cost_pe) / 20 % 12)
-                    + " pennies: " + ((insert_pe - cost_pe) % 20);
-        else
-            result = cost_pe > insert_pe ? "> Ошибка!!!" : "> Сдачи нет!!!";
-
-        System.out.println(result);
+                    + " pennies: " + ((insert_pe - cost_pe) % 20)
+        :
+            cost_pe > insert_pe ? "> Ошибка!!!" : "> Сдачи нет!!!");
     }
 }
